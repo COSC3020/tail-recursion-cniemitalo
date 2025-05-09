2c89337292ca27080d60de6b2fc5122ab56025f1
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-05-08 21:31:43 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 function tFib(n, fib, fibOld) {
     //return the final value 
-    //ends here because we've already checked first two cases
-    if (n === 2) return fib; 
+    //ends here because we start at base case 1 
+    if (n === 1) return fib; 
 
     //calculate the previous two numbers 
     //similar to iterative 
@@ -16,8 +16,7 @@ function tFib(n, fib, fibOld) {
 
 function fib(n) {
     //base cases
-    if (n === 0) return 0; 
-    if (n === 1) return 1; 
+    if (n === 0) return 0;  
 
     //start with base cases 0, 1 and build up from there
     return tFib(n, 1, 0); 
